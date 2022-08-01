bc2e065f69b563f49b0bb49883e5c52498c1d46e
jh901 <jh901794@gmail.com>
2022-08-01 17:42:27 +0900

    newInstance

---
+++ src/main/java/j20220801/Mega2/Gwang_2.java
@@ -0,0 +1,13 @@
+package j20220801.Mega2;
+
+public class Gwang_2 implements Mega2{
+
+    public void Room(){
+        System.out.println("2¹Ý");
+    }
+
+    @Override
+    public void ChoiceRoom() {
+        System.out.println("2¹Ý");
+    }
+}
---
+++ src/main/java/j20220801/Mega2/Mega2.java
@@ -0,0 +1,7 @@
+package j20220801.Mega2;
+
+public interface Mega2 {
+    //1반부터 5반까지 찾는다.
+    public void ChoiceRoom();
+
+}
